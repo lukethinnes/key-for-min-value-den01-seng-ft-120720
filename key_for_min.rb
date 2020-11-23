0cad39a4d45666c5a8d4dd25f88c1feaef3367b6
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-23 19:33:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  newArr = []
   name_hash.each do |key, value|
-    newArr = []
     if value.is_a? numeric
-      newArr.push
+      value = newArr.push
     end
     newArr.sort
     return newArr[0]
